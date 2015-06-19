17a74b64019d21c8e5929c2af1d5f6dcd7397e65
Spenser Hannon <spenson66@gmail.com>
2015-06-19 11:27:10 -0500

    updated css for full rooms

--- newRooms.user.js
+++ newRooms.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Monster Minigame Room Monitor
 // @namespace https://github.com/spAnser/steamSummerMinigame_newRooms
-// @version 1.1.1
+// @version 1.2
 // @description Monitors new Rooms for the Monster Minigame
 // @author spAnser
 // @match *://steamcommunity.com/minigame*
@@ -11,10 +11,10 @@
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
 
-window.lastRoomCheck = 47000 // This is the room the scripts starts of looking from
-window.refreshSpeed = 2000 // 2000 is 2 seconds and should be fine
-window.levelThreshhold = 100 // Any game that is over this level will be hidden automatically
-window.checkSize = 10 // 10 rooms should be plenty to check at one time
+var lastRoomCheck = 47010 // This is the room the scripts starts of looking from
+var refreshSpeed = 500 // 2000 is 2 seconds and should be fine
+var levelThreshhold = 100 // Any game that is over this level will be hidden automatically
+var checkSize = 10 // 10 rooms should be plenty to check at one time
 
 // https://github.com/doodlely/numfuzz/blob/master/src/numfuzz.js
 !function(){"use strict";var t={};t.fuzzy=function(t){return"number"!=typeof t&&(t=parseInt(t,10)),Math.abs(t)>=1e6?this.round(t/1e6,1)+"M":Math.abs(t)>=1e3?this.round(t/1e3,1)+"k":t.toString()},t.round=function(t,n){var u=Number((Math.floor(10*t)/10).toFixed(n));return u.toFixed(u%1===0?0:1)},window.NumFuzz=t}(),function(t){t.fn.numFuzz=function(){this.each(function(n,u){var e=parseInt(t(u).text(),10),r=NumFuzz.fuzzy(e);t(u).attr("title",e.toLocaleString()).text(r).data("numfuzz-val",e)})}}(jQuery);
@@ -39,11 +39,17 @@ addGlobalStyle('#newRooms{position:absolute;top:6px;left:0;width:100%;z-index:10
 addGlobalStyle('#newRooms .main_btn{background-color: #884691;color: #ee7aff;border: 2px solid #a554b1;font-size:8px;line-height:16px;width:66px;margin:2px 0 5px;text-align:center;cursor:pointer;display:inline-block;text-shadow:1px 1px 0 rgba(0,0,0,.5)}')
 
 addGlobalStyle('.room.started{background-color:#8aaf05;color:#d9ff54;border:2px solid #a3cf06;text-shadow:1px 1px 0 rgba(0,0,0,.5)}')
+addGlobalStyle('.room.full{background-color:#af0535;color:#ff5483;border:2px solid #cf063e;text-shadow:1px 1px 0 rgba(0,0,0,.5)}')
 addGlobalStyle('.room.filling{background-color:#af7f05;color:#ffd054;border:2px solid #cf9706;text-shadow:1px 1px 0 rgba(0,0,0,.5)}')
 addGlobalStyle('.room.soon{background-color:#5c5b58;color:#b0aca3;border:2px solid #6d6b68;text-shadow:1px 1px 0 rgba(0,0,0,.5)}')
+addGlobalStyle('#scanningRooms{font-size: 13px;color: #fff;text-align: center;font-family: Arial, Helvetica, Verdana, sans-serif;line-height: 17px;text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.3);}')
 
 $J('.section_overview').prepend(
     $J('<div id="newRooms"></div>')
+)
+
+$J('.section_play').append(
+    $J('<div id="scanningRooms"></div>')
 )
 
 function addRoom(room, size, level, watch) {
@@ -81,12 +87,12 @@ function addRoom(room, size, level, watch) {
         $J('<a href="Javascript:JoinGame(' + room + ')" class="main_btn">Join</a>')
     )
 
-    if (level < 0)
+    if (size >= 1500)
+        _room.addClass('full')
+    else if (level < 0)
         _room.addClass('soon')
     else if (level < 1)
         _room.addClass('filling')
-    else if (size >= 1500)
-        _room.addClass('full')
     else
         _room.addClass('started')
      if (level > levelThreshhold)
@@ -119,6 +125,7 @@ function checkRoom(room, watch) {
             $J('#newRooms').sortDivs()
         }
     });
+    $J('#scanningRooms').html('Scanning Room: ' + lastRoomCheck)
 }
 
 function checkRooms(start, range) {
